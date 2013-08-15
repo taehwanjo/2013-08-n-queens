04338b7d185930be49f2973c4ec3f3e86422b0af
Hack Reactor Students <github@hackreactor.com>
2013-08-14 18:00:19 -0700

    findNRookSolution passes first test

--- src/solvers.js
+++ src/solvers.js
@@ -3,8 +3,25 @@
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
 window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
+  // var solution = undefined;
+  // n = number of rooks
+   // check i to 'n'
+  // place rooks in indexes 0 to n
+  // step 2: we deduct the threatened squares from possible solutions
+  // recurse on remaining squares.
 
+//var x = makeEmptyMatrix(this.get('n'));
+  var columnStart = 0;
+  var solution = new Board({'n':n});
+    for(var x = 0; x < n; x++) { //explores 4 possibilities on a 4x4 board (x represents first 4 positions)
+      for (var i = columnStart; i < n; i++) { //checks out every square
+        if(!solution.hasAnyRowConflicts && !solution.hasAnyColConflicts) {
+          solution.togglePiece(x,i); //x is rowIndex, and i is columnIndex
+          i = n; //can no longer put piece on this row
+        }
+      columnStart++;
+      }
+    }
   console.log('Single solution for ' + n + ' rooks:', solution);
   return solution;
 };
